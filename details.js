5d4f59599c28da6f1399fffc52efa4c508d48130
Rodrigo Santos <119634097+xRodrigoSx@users.noreply.github.com>
2023-12-28 12:18:21 -0300

    Add files via upload

--- details.js
+++ details.js
@@ -8,14 +8,45 @@ document.addEventListener('DOMContentLoaded', () => {
         const resp = await fetch(url);
         const data = await resp.json();
 
+        const colors = {
+            grass: '#7AC74C',
+            water: '#6390F0',
+            fire: '#EE8130',
+            electric: '#F7D02C',
+            normal: '#A8A77A',
+            bug: '#A6B91A',
+            poison: '#A33EA1',
+            ground: '#E2BF65',
+            rock: '#B6A136',
+            fairy: '#D685AD',
+            dragon: '#6F35FC',
+            psychic: '#F95587',
+            flying: '#A98FF3',
+            fighting: '#C22E28',
+            steel: '#B7B7CE',
+            ice: '#96D9D6',
+            ghost: '#735797',
+            dark: '#705746'
+        }
+
+        const mainType = data.types[0].type.name;
+
+        // Get the background color based on the main type
+        const color = colors[mainType] || '#ffffff';
+
         // Create HTML content for Pokemon details
         const pokemonDetailsHTML = `
-            <h2>${data.name[0].toUpperCase() + data.name.slice(1)}</h2>
-            <p>ID: #${data.id}</p>
-            <p>Type: ${data.types.map(type => type.type.name).join(', ')}</p>
-            <!-- Add more details as needed -->
-
-            <button onclick="goBack()">Go Back</button>
+        <div style="background-color: ${color}; padding: 20px;">
+        <button onclick="goBack()"><</button>
+        <div class="name">
+        <h2>${data.name[0].toUpperCase() + data.name.slice(1)}</h2>
+        <p>#${data.id}</p>
+        </div>
+        <p>${data.types.map(type => `<span class="tipo">${type.type.name}</span>`).join('')}</p>
+        <div class="pokemon">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png" alt="${name}">
+        </div>
+        </div>
         `;
 
         // Display details in the container
